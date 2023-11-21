b0d17cf91fe319ec99cd617e72cec9b2488c556d
cliff <cliffmudigo5@gmail.com>
2023-11-21 18:02:44 +0300

    heroku

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.post("/", (req, res) => {
     request.end();
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is running on port 3000");
 });
